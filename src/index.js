16bf8d87d81ca3653abde762337d118930bff326
hugoherculano <hugohercullano@gmail.com>
2023-02-05 21:02:09 -0300

    Challenge completed of Rocketseat :rocket:

--- src/index.js
+++ src/index.js
@@ -10,19 +10,67 @@ app.use(cors());
 const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+
+  const userAlreadyExists = users.find(user => user.username === username);
+
+  if(!userAlreadyExists) {
+    return response.status(404).json({ error: 'User not found!'});
+  }
+
+  request.user = userAlreadyExists;
+
+  return next();
 }
 
 function checksCreateTodosUserAvailability(request, response, next) {
-  // Complete aqui
+  const { user } = request;
+
+  if(user.pro || user.todos.length < 10) {
+    return next();
+  }
+
+  return response.status(403).json({ error: "Don't have permission!" })
 }
 
 function checksTodoExists(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+  const { id } = request.params;
+
+  const user = users.find(user => user.username === username);
+
+  if(!user) {
+    return response.status(404).json({ error: 'User not found!'});
+  }
+
+  if(!validate(id)) {
+    return response.status(400).json({ error: 'Id invalided'});
+  }
+
+  const todo = user.todos.find(todo => todo.id === id);
+
+  if(!todo) {
+    return response.status(404).json({ error: 'Todo not found!'});
+  }
+
+  request.todo = todo;
+  request.user = user;
+
+  return next()
 }
 
 function findUserById(request, response, next) {
-  // Complete aqui
+  const { id } = request.params;
+
+  const userAlreadyExists = users.find(user => user.id === id);
+
+  if(!userAlreadyExists) {
+    return response.status(404).json({ error: 'User not found!'});
+  }
+
+  request.user = userAlreadyExists;
+
+  return next();
 }
 
 app.post('/users', (request, response) => {
